e778aeb19fc21f773ffa9d510f058b9ffc705a1c
PouryaCE <PouryaJanparvar@yahoo.com>
2023-03-11 10:56:47 +0330

    UserChangeForm for user model

--- ecommerce/accounts/forms.py
+++ ecommerce/accounts/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.core.exceptions import ValidationError
+from django.contrib.auth.forms import ReadOnlyPasswordHashField
 
 from .models import User
 
@@ -32,3 +33,28 @@ class UserCreationForm(forms.ModelForm):
         return password2
 
 
+class UserChangeForm(forms.ModelForm):
+    password = ReadOnlyPasswordHashField()
+
+    class Meta:
+        model = User
+        fields = "__all__"
+
+    def clean_phone_number(self):
+        phone_number = self.cleaned_data["phone_number"]
+        if User.objects.get(phone_number=phone_number).exists():
+            return ValidationError("کاربری با این شماره تلفن قبلا ثبت شده است")
+        return phone_number
+
+    def clean_email(self):
+        email = self.cleaned_data["email"]
+        if User.objects.get(email=email).exists():
+            return ValidationError("کاربری با این ایمیل قبلا ثبت شده است")
+        return email
+
+    def clean_password2(self):
+        password1 = self.cleaned_data["password1"]
+        password2 = self.cleaned_data["password2"]
+        if password2 and password1 and password1 != password2:
+            return ValidationError("پسورد ها باید یکسان باشند")
+        return password2
